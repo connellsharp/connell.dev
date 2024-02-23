1c26dabfa532ec5770b9cd1588b460130af8c0c3
Connell <connellsharp@gmail.com>
2024-02-23 00:24:42 +0000

    Add basic theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import '../styles/reset.css';
+    import '../styles/themes.css';
+    import '../styles/global.css';
+</script>
+
+<style>
+    
+</style>
+
+<div class="layout">
+    <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    h1 { font-size: 2.5em; }
+</style>
+
+<h1>Connell</h1>
